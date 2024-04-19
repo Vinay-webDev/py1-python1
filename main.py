1996ba68ca5ef852886e9ba6d8ff17fe8ea6d604
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-19 23:38:22 +0530

    getting started with python🐍

---
+++ main.py
@@ -0,0 +1,72 @@
+# variable = a container for a value. it behaves as the value that it contains
+
+#first_name = "bro"
+#last_name = "dude"
+#full_name = first_name + last_name
+#print(full_name)
+#print (name)
+#print ("Hello " +name)
+#print (type(first_name))
+#print("Hello " + full_name)
+
+#age = 23
+#age = age + 1
+#print (age)
+#print ( type (age))
+#print("Hello "+age)
+#print("your age is :"+str(age))
+
+#weight = 56.5
+#print(weight)
+#print("your weight is: "+str(weight)+"KG")
+
+#boolean data type
+#human = True
+#print(type(human))
+#print("Are u human: "+str(human))
+
+#multiple assignment = it allows us to assign multiple variables at the same time in one line of code
+
+#name = "dude"
+#age = 23
+#human = True
+#print(name)
+#print(age)
+#print(human)
+
+#name,age,human ="dude",23,True
+#print(name)
+#print(age)
+#print(human)
+
+
+#lol = 100
+#nigga = 100
+#gey = 100
+#retard = 100
+
+#lol=nigga=gey=retard = 100
+#print(lol)
+#print(nigga)
+#print(gey)
+#print(retard)
+
+#string methods
+
+#name = "hello dude"
+#print(len(name))
+#print(name.find("d"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("o"))
+#print(name.replace("l","o"))
+#print(name*8)
+
+
+
+
+
+
